fb4e38e0616b8038248d97fdc15e40345de6e056
Santiago Urban <urbansanty@gmail.com>
2022-11-21 14:40:25 -0300

    Inventory manage system

---
+++ inventoySystem/inventory/forms.py
@@ -0,0 +1,13 @@
+from django.forms import ModelForm
+from .models import Inventory
+
+class AddIventoryForm(ModelForm):
+    class Meta:
+        model = Inventory
+        fields = ['name', 'cost_per_item', 'quantity_in_stock', 'quantity_sold']
+        
+        
+class UpdateInventoryForm(ModelForm):
+    class Meta:
+        model = Inventory
+        fields = ['name', 'cost_per_item', 'quantity_in_stock', 'quantity_sold']
