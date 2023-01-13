69261bccfdce925e217b9a80187ff314928f66c1
santiudev <santiagourban26@gmail.com>
2023-01-13 13:58:40 -0300

    docker

--- inventoySystem/inventory/urls.py
+++ inventoySystem/inventory/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import inventory_list, per_product_view, add_product, delete_inventory, update_inventory, dashboard
+from .views import inventory_list, per_product_view, add_product, delete_inventory, update_inventory
+#dashboard
 
 urlpatterns = [
     path("", inventory_list, name="inventory_list"),
@@ -7,7 +8,7 @@ urlpatterns = [
     path("add_inventory/", add_product, name="add_inventory"),
     path("delete/<int:pk>/", delete_inventory, name="delete_inventory"),
     path("update/<int:pk>/", update_inventory, name="update_inventory"),
-    path("dashboard/", dashboard, name="dashboard"),
+    #path("dashboard/", dashboard, name="dashboard"),
 ]
 
 
